69e4c8fbf8ebb922c6ec91e2f8595d8cee999048
Hisaki Akaza <4494300+TheRed@users.noreply.github.com>
2018-08-11 16:00:35 +0900

    feat: scaffold goqoo.configへの自動追記を実装

--- generators/scaffold/index.js
+++ generators/scaffold/index.js
@@ -1,6 +1,15 @@
 'use strict'
 const Generator = require('yeoman-generator')
+const fs = require('fs')
+const chalk = require('chalk')
+const yaml = require('js-yaml')
 
+const padding = str => ('         ' + str).substr(-9) + ' '
+const coloredMessage = (color, fileStatus, relativePath) => chalk[color](padding(fileStatus)) + relativePath
+const message = {
+  error: m => coloredMessage('red', 'Error', m),
+  force: m => coloredMessage('yellow', 'force', m),
+}
 module.exports = class extends Generator {
   configuring () {
     this.appName = this.args[0]
@@ -8,6 +17,20 @@ module.exports = class extends Generator {
 
   writing () {
     const appName = this.appName
+
+    // Define app name
+    const configRelativePath = 'config/goqoo.config.yml'
+    const config = yaml.safeLoad(this.fs.read(this.destinationPath(configRelativePath)))
+    if (Object.values(config.apps).includes(appName)) {
+      this.log(message.error(`App '${appName}' already exists!`))
+      process.exit(1)
+    }
+    config.apps.push(appName)
+    const configStr = yaml.safeDump(config)
+    fs.writeFileSync(this.destinationPath(configRelativePath), configStr)
+    this.log(message.force(configRelativePath))
+
+    // Create JS source code
     ;['', '-index', '-detail', '-submit'].forEach(eventHandler =>
       this.fs.copyTpl(
         this.templatePath(`scaffold${eventHandler}.js`),
