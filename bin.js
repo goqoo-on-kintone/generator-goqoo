73e34ba1cb75c4bdaa0e09087a4e71736d910244
Hisaki Akaza <4494300+TheRed@users.noreply.github.com>
2018-10-10 22:06:31 +0900

    feat: goqooコマンドをyo goqooのラッパーとして使う処理を実装

---
+++ bin.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+'use strict'
+const { spawnSync } = require('child_process')
+
+const [subCommand, ...args] = process.argv.slice(2)
+let subGenerator
+
+switch (subCommand) {
+  case 'new':
+    subGenerator = 'app'
+    break
+  case 'generate':
+    subGenerator = args.shift()
+    break
+  default:
+    process.exit(1)
+}
+
+const yeoman = spawnSync('yo', [`goqoo:${subGenerator}`, ...args], { stdio: 'inherit' })
+process.exit(yeoman.code)
