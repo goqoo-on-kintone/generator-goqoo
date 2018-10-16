e6610257551257acad30a7ad1b78df44636d2711
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2018-10-16 12:10:35 +0900

    feat: S3への自動アップロードとwebpack-dev-serverに対応

--- generators/app/templates/static/webpack.config.js
+++ generators/app/templates/static/webpack.config.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-console */
+const webpack = require('webpack')
 const path = require('path')
 const rcfile = require('rc-config-loader')
 require('dotenv').config({ path: path.resolve('config/.env') })
 const DropboxKintone = require('./.goqoo/dropbox')
+const S3Plugin = require('webpack-s3-plugin')
 
 const { npm_package_name: projectName } = process.env
 const { apps, useDropbox } = rcfile('goqoo', { configFileName: `${__dirname}/config/goqoo.config` }).config
@@ -40,10 +42,10 @@ if (useDropbox) {
   }
 }
 
-module.exports = {
+const config = {
   entry,
   output,
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   module: {
     rules: [
       {
@@ -82,4 +84,39 @@ module.exports = {
       vue$: 'vue/dist/vue.esm.js',
     },
   },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    inline: true,
+    // hot: true,
+    https: true,
+    port: 8888,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+  },
 }
+
+if (process.env.S3) {
+  config.entry = Object.entries(config.entry).reduce((obj, [key, value]) => {
+    obj[`${key}-${process.env.AWS_RANDOM_SUFFIX}`] = value
+    return obj
+  }, {})
+
+  config.plugins.push(
+    new S3Plugin({
+      // Exclude uploading of html
+      exclude: /.*\.html$/,
+      // s3Options are required
+      s3Options: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_S3_REGION,
+      },
+      s3UploadOptions: {
+        Bucket: process.env.AWS_S3_BUCKET,
+      },
+      basePath: projectName,
+    })
+  )
+}
+
+module.exports = config
