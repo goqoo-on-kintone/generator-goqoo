7b51a41d9e3af00c73588e5bffbabd06135849c3
Isao Yoshikoshi <latica.jp@gmail.com>
2019-04-08 10:01:13 +0900

    fix/on-edits-show

--- generators/g-scaffold/templates/on-edits-show.js
+++ generators/g-scaffold/templates/on-edits-show.js
@@ -1,3 +1,3 @@
-kintone.events.on(['app.record.create.submit', 'app.record.edit.submit', 'app.record.index.edit.submit'], event => {
+kintone.events.on(['app.record.create.show', 'app.record.edit.show', 'app.record.index.edit.show'], event => {
   return event
 })
