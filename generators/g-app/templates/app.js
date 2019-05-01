4c380c4b04489e902b5971a63bf7839dc307526b
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2019-05-02 02:32:01 +0900

    fix: g-appのswalも2.xに修正

--- generators/g-app/templates/app.js
+++ generators/g-app/templates/app.js
@@ -1,12 +1,10 @@
-import swal from 'sweetalert2'
+import swal from 'sweetalert'
 import img from '../../.goqoo/img/SmallLogo.jpg'
 
 kintone.events.on('app.record.index.show', event => {
   swal({
-    text: 'Hello,  Goqoo on kintone!',
-    confirmButtonText: 'OK',
-    imageUrl: img,
-    imageHeight: '200',
+    text: 'Hello, Goqoo on kintone!',
+    icon: img,
   })
 
   return event
